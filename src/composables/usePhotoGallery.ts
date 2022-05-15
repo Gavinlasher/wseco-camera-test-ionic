5caccada53aae72d5aeac60d6cf060202de25091
gavinlasher <krgavin21@yahoo.com>
2022-05-15 12:06:41 -0600

    commit

--- src/composables/usePhotoGallery.ts
+++ src/composables/usePhotoGallery.ts
@@ -12,10 +12,8 @@ export function usePhotoGallery() {
       quality: 100,
     });
     const fileName = new Date().getTime() + '.jpeg';
-const savedFileImage = {
-  filepath: fileName,
-  webviewPath: photo.webPath,
-};
+const savedFileImage = await savePicture(photo,  fileName)
+
 photos.value = [savedFileImage, ...photos.value];
   };
   return {
@@ -26,3 +24,34 @@ export interface UserPhoto {
   filepath: string;
   webviewPath?: string;
   }
+
+  const convertBlobToBase64 = (blob: Blob) =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
+  const savePicture = async (photo: Photo, fileName: string): Promise<UserPhoto> => {
+    let base64Data: string;
+  
+    // Fetch the photo, read as a blob, then convert to base64 format
+    const response = await fetch(photo.webPath!);
+    const blob = await response.blob();
+    base64Data = (await convertBlobToBase64(blob)) as string;
+  
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data,
+    });
+  
+    // Use webPath to display the new image instead of base64 since it's
+    // already loaded into memory
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath,
+    };
+  };
