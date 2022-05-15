71827ebd3cf1c266d8cb8d6a148a329f23b46698
gavinlasher <krgavin21@yahoo.com>
2022-05-15 10:59:41 -0600

    commit

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -2,28 +2,70 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 2</ion-title>
+        <ion-title>Vue Camera App</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
-        </ion-toolbar>
-      </ion-header>
+     <ion-fab vertical="bottom" horizontal="center" slot="fixed" >   
+<ion-fab-button @click="takePhoto()">
+<ion-icon :icon="camera"> 
+
+</ion-icon>
+</ion-fab-button>
+     </ion-fab>
       
-      <ExploreContainer name="Tab 2 page" />
+     
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
+import {camera,trash, close} from "ionicons/icons"
 import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import {
+  IonPage,
+  IonHeader,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+} from '@ionic/vue';
+import { setupConfig } from "@ionic/core";
+import  {usePhotoGallery} from "@/composables/usePhotoGallery"
 
-export default defineComponent({
+
+export default {
   name: 'Tab2Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
-});
+components: {
+    IonPage,
+    IonHeader,
+    IonFab,
+    IonFabButton,
+    IonIcon,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonImg,
+  },
+
+setup(){
+  const {takePhoto} = usePhotoGallery();
+  return{
+camera, trash,close, takePhoto
+  }
+}
+
+
+}
+
+
 </script>
